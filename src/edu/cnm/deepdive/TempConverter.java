ebbc0fc763a3e6ad3b6e05b0e5f2ca2bac95cd79
Jacqueline Martin <jacquimartin505@gmail.com>
2019-09-18 09:39:18 -0600

    Support exhausted standard input.

--- src/edu/cnm/deepdive/TempConverter.java
+++ src/edu/cnm/deepdive/TempConverter.java
@@ -3,49 +3,53 @@ package edu.cnm.deepdive;
 import java.util.*;
 
 public class TempConverter {
-    private static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        convert(input);
-        }
+  private static Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    convert(input);
+  }
 
 
-
-    private static void convert(Scanner scanner) {
-        String line;
-        while (!(line = scanner.nextLine().trim()).isEmpty()) {
-            char finalChar =line.toUpperCase().charAt(line.length() -1);
+  private static void convert(Scanner scanner) {
+      try {
+          String line;
+          while (!(line = scanner.nextLine().trim()).isEmpty()) {
+            char finalChar = line.toUpperCase().charAt(line.length() - 1);
             if (finalChar == 'C') {
-                double celsius = parseTemperature(line);
-                System.out.printf("%.3fF%n", celsiusToFahrenheit(celsius));
+              double celsius = parseTemperature(line);
+              System.out.printf("%.3fF%n", celsiusToFahrenheit(celsius));
 
 
             } else if (finalChar == 'F') {
-                double fahrenheit = parseTemperature(line);
-                System.out.printf("%.3fC%n", celsiusToFahrenheit(fahrenheit));
+              double fahrenheit = parseTemperature(line);
+              System.out.printf("%.3fC%n", celsiusToFahrenheit(fahrenheit));
             }
-        }
-    }
+          }
+      } catch (NoSuchElementException e) {
+          // Ignore exception input is exhausted.
+      }
+  }
 
-    private static double parseTemperature(String line) {
-        line = line.substring(0, line.length() - 1).trim();
-        return Double.parseDouble(line);
-    }
+  private static double parseTemperature(String line) {
+    line = line.substring(0, line.length() - 1).trim();
+    return Double.parseDouble(line);
+  }
 
-    public static double celsiusToFahrenheit(double celsius) {
-        double fahrenheit;
-        
-        fahrenheit = 9 * celsius / 5 + 32;
-        
-        return fahrenheit;
-    }
-    
-    public static double fahrenheitToCelsius(double fahrenheitInput) {
-        double celsius;
+  public static double celsiusToFahrenheit(double celsius) {
+    double fahrenheit;
 
-        double fahrenheit = input.nextDouble();
-        celsius = (fahrenheit - 32) * 5 / 9;
-        
-        return celsius;
-    }
+    fahrenheit = 9 * celsius / 5 + 32;
+
+    return fahrenheit;
+  }
+
+  public static double fahrenheitToCelsius(double fahrenheitInput) {
+    double celsius;
+
+    double fahrenheit = input.nextDouble();
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    return celsius;
+  }
 }
